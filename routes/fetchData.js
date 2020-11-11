c8b9b971251a7bead157c940d879275567a5b4fe
zhangw <786014381@qq.com>
2020-11-11 16:10:16 +0800

    feat 增加元数据分类

--- routes/fetchData.js
+++ routes/fetchData.js
@@ -6,46 +6,60 @@ const classDao = require('../dao/foodClassDao');
 const foodDataDao = require('../dao/foodDataDao');
 const dicDao = require('../dao/dicDao');
 
+const elementClassDic = [
+    { code: 'C1', name: '能量与相关成分' },
+    { code: 'C2', name: '维生素' },
+    { code: 'C3', name: '矿物质' },
+    { code: 'C4', name: '脂肪酸' },
+];
 const elementDic = [
-    { code: 'Edible', name: '食部' },
-    { code: 'Water', name: '水分' },
-    { code: 'Energy', name: '能量' },
-    { code: 'Protein', name: '蛋白质' },
-    { code: 'Fat', name: '脂肪' },
-    { code: 'Cholesterol', name: '胆固醇' },
-    { code: 'Ash', name: 'Ash' },
-    { code: 'CHO', name: '碳水化合物' },
-    { code: 'DietaryFiber', name: '总膳食纤维' },
-    { code: 'Carotene', name: '胡萝卜素' },
-    { code: 'Vitamin', name: '维生素A' },
-    { code: 'αTE', name: 'α-TE' },
-    { code: 'Thiamin', name: '硫胺素' },
-    { code: 'Riboflavin', name: '核黄素' },
-    { code: 'Niacin', name: '烟酸' },
-    { code: 'VitaminC', name: '维生素C' },
-    { code: 'Ca', name: '钙' },
-    { code: 'P', name: '磷' },
-    { code: 'K', name: '钾' },
-    { code: 'Na', name: '钠' },
-    { code: 'Mg', name: '镁' },
-    { code: 'Fe', name: '铁' },
-    { code: 'Zn', name: '锌' },
-    { code: 'Se', name: '硒' },
-    { code: 'Cu', name: '铜' },
-    { code: 'Mn', name: '锰' },
-    { code: 'I', name: '碘' },
-    { code: 'SFA', name: '饱和脂肪酸' },
-    { code: 'MUFA', name: '单不饱和脂肪酸' },
-    { code: 'PUFA', name: '多不饱和脂肪酸' },
+    { code: 'Edible', name: '食部', class: 'C1' },
+    { code: 'Water', name: '水分', class: 'C1' },
+    { code: 'Energy', name: '能量', class: 'C1' },
+    { code: 'Protein', name: '蛋白质', class: 'C1' },
+    { code: 'Fat', name: '脂肪', class: 'C1' },
+    { code: 'Cholesterol', name: '胆固醇', class: 'C1' },
+    { code: 'Ash', name: '灰分', class: 'C1' },
+    { code: 'CHO', name: '碳水化合物', class: 'C1' },
+    { code: 'DietaryFiber', name: '总膳食纤维', class: 'C1' },
+    { code: 'Carotene', name: '胡萝卜素', class: 'C2' },
+    { code: 'Vitamin', name: '维生素A', class: 'C2' },
+    { code: 'αTE', name: 'α-TE', class: 'C2' },
+    { code: 'Thiamin', name: '硫胺素', class: 'C2' },
+    { code: 'Riboflavin', name: '核黄素', class: 'C2' },
+    { code: 'Niacin', name: '烟酸', class: 'C2' },
+    { code: 'VitaminC', name: '维生素C', class: 'C2' },
+    { code: 'Ca', name: '钙', class: 'C3' },
+    { code: 'P', name: '磷', class: 'C3' },
+    { code: 'K', name: '钾', class: 'C3' },
+    { code: 'Na', name: '钠', class: 'C3' },
+    { code: 'Mg', name: '镁', class: 'C3' },
+    { code: 'Fe', name: '铁', class: 'C3' },
+    { code: 'Zn', name: '锌', class: 'C3' },
+    { code: 'Se', name: '硒', class: 'C3' },
+    { code: 'Cu', name: '铜', class: 'C3' },
+    { code: 'Mn', name: '锰', class: 'C3' },
+    { code: 'I', name: '碘', class: 'C3' },
+    { code: 'SFA', name: '饱和脂肪酸', class: 'C4' },
+    { code: 'MUFA', name: '单不饱和脂肪酸', class: 'C4' },
+    { code: 'PUFA', name: '多不饱和脂肪酸', class: 'C4' },
 ];
 
 router.get('/fetchData', async (req, res, next) => {
     let classInfo = await getCategoryInfo();
     await classDao.delClass();
     await classDao.saveClass(classInfo.classToplist);
-    elementDic.forEach((v) => (v.group = 'element'));
+    elementDic.forEach((v, i) => {
+        v.group = 'element';
+        v.sn = i + 1;
+    });
+    elementClassDic.forEach((v, i) => {
+        v.group = 'elementClass';
+        v.sn = i + 1;
+    });
     await dicDao.delDic();
     await dicDao.saveDic(elementDic);
+    await dicDao.saveDic(elementClassDic);
     for (let i = 0; i < classInfo.classlist.length; i++) {
         let v = classInfo.classlist[i];
         await getFoodInfo(v);
@@ -73,7 +87,8 @@ async function saveFoodInfo(info, data) {
     let saveTasks = [];
     data.forEach((v) => {
         let curItem = {
-            classCode: info.classCode,
+            classCode: info.classTopCode,
+            className: info.classTopName,
             info: info.className,
             code: v[0],
             name: v[2],
@@ -131,10 +146,11 @@ async function getCategoryInfo() {
         let classes = $(boxs[i]).find('a');
         let classTopCode = classes[0].attribs.data_id;
         let classTopName = classes[0].lastChild.data;
-        classToplist.push({ code: classTopCode, name: classTopName });
+        classToplist.push({ code: classTopCode, name: classTopName, sn: i + 1 });
         for (let j = 1; j < classes.length; j++) {
             classlist.push({
                 classTopCode,
+                classTopName,
                 classCode: classes[j].attribs.data_id,
                 className: classes[j].lastChild.data,
             });
