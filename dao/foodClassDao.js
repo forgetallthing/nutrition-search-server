b243908c0b948f68e407c08f2cf1848360a004cd
zhangw <786014381@qq.com>
2020-11-13 11:09:52 +0800

    feat 增加获取元数据接口

--- dao/foodClassDao.js
+++ dao/foodClassDao.js
@@ -2,6 +2,10 @@ function getClassCollection() {
     return global.mongodb.collection('food_class');
 }
 
+function getDicCollection() {
+    return global.mongodb.collection('dic');
+}
+
 async function saveClass(data) {
     await getClassCollection().insertMany(data);
 }
@@ -18,8 +22,17 @@ async function findClass(filter, cols) {
         .toArray();
 }
 
+async function findDic(filter, cols) {
+    return await getDicCollection()
+        .find(filter || {})
+        .project(cols || { _id: 0 })
+        .sort({ sn: 1 })
+        .toArray();
+}
+
 module.exports = {
     saveClass,
     delClass,
     findClass,
+    findDic,
 };
--- routes/foodClass.js
+++ routes/foodClass.js
@@ -3,6 +3,34 @@ const router = express.Router();
 const foodClassDao = require('../dao/foodClassDao');
 const { classMap } = require('../common/classMap');
 
+router.get('/getElementClass', async function (req, res, next) {
+    let list = await foodClassDao.findDic(
+        {
+            group: 'elementClass',
+        },
+        {
+            _id: 0,
+            group: 0,
+            sn: 0,
+        }
+    );
+    res.send({ list });
+});
+
+router.get('/getElement', async function (req, res, next) {
+    let list = await foodClassDao.findDic(
+        {
+            group: 'element',
+        },
+        {
+            _id: 0,
+            group: 0,
+            sn: 0,
+        }
+    );
+    res.send({ list });
+});
+
 router.get('/getClass', async function (req, res, next) {
     let list = await foodClassDao.findClass();
     list.forEach((v) => {
