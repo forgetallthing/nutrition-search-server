91c0b8728df430bde18b1126fae5bce9028ed087
wdpl <811197627@qq.com>
2020-11-23 18:41:43 +0800

    fix

--- routes/food.js
+++ routes/food.js
@@ -3,56 +3,66 @@ const router = express.Router();
 const foodDataDao = require('../dao/foodDataDao');
 
 router.get('/getList', async function (req, res, next) {
-    let p = req.query;
-    p.lastValue = parseInt(p.lastValue);
-    p.elements = JSON.parse(p.elements || '[]');
-    let foodList = await foodDataDao.findPage(p);
-    let list = [];
-    let foodCodes = [];
-    foodList.forEach((v) => {
-        let obj = { eles: {} };
-        for (const key in v) {
-            if (p.elements.includes(key)) {
-                obj.eles[key] = v[key];
-            } else {
-                obj[key] = v[key];
-            }
-        }
-        list.push(obj);
-    });
-
-    if (p.userId && !p.collect) {
-        let collectList = await foodDataDao.findCollectByFoods(p.userId, foodCodes);
-        foodList.forEach((v) => {
-            if (collectList.find((val) => val.foodCode === v.code)) v.isCollect = 1;
-        });
+  let p = req.query;
+  p.lastValue = parseInt(p.lastValue);
+  p.elements = JSON.parse(p.elements || '[]');
+  let foodList = await foodDataDao.findPage(p);
+  let list = [];
+  let foodCodes = [];
+  foodList.forEach((v) => {
+    let obj = { eles: {} };
+    for (const key in v) {
+      if (p.elements.includes(key)) {
+        obj.eles[key] = v[key];
+      } else {
+        obj[key] = v[key];
+      }
     }
-    res.send({ list });
+    foodCodes.push(obj.code);
+    list.push(obj);
+  });
+
+  if (p.userId && !p.collect) {
+    let collectList = await foodDataDao.findCollectByFoods(p.userId, foodCodes);
+    list.forEach((v) => {
+      if (collectList.find((val) => val.foodCode === v.code)) {
+        v.isCollect = 1;
+      } else {
+        v.isCollect = 0;
+      }
+    });
+  }
+  res.send({ list });
 });
 
 router.get('/getfoodInfo', async function (req, res, next) {
-    let p = req.query;
-    let foodCode = JSON.parse(p.code);
-    let info = await foodDataDao.findFoodInfo({ code: foodCode });
-    if (p.userId) {
-        if (await foodDataDao.findCollectByFood(p.userId, foodCode)) info.isCollect = 1;
+  let p = req.query;
+  let foodCode = JSON.parse(p.code);
+  let info = await foodDataDao.findFoodInfo({ code: foodCode });
+  if (p.userId) {
+    const isCollect = await foodDataDao.findCollectByFood(p.userId, foodCode);
+    if (isCollect) {
+      info.isCollect = 1;
+    } else {
+      info.isCollect = 0;
     }
+  }
 
-    res.send({ info });
+  res.send({ info });
 });
 
 router.get('/collectFood', async function (req, res, next) {
-    let p = req.query;
-    let foodCode = JSON.parse(p.code);
-    await foodDataDao.saveCollectFood(p.userId, foodCode);
-    res.send({});
+  let p = req.query;
+  let foodCode = JSON.parse(p.code);
+  await foodDataDao.saveCollectFood(p.userId, foodCode);
+  res.send({});
 });
 
 router.get('/unCollectFood', async function (req, res, next) {
-    let p = req.query;
-    let foodCode = JSON.parse(p.code);
-    await foodDataDao.removeCollectByFood(p.userId, foodCode);
-    res.send({});
+  let p = req.query;
+  let foodCode = JSON.parse(p.code);
+  await foodDataDao.removeCollectByFood(p.userId, foodCode);
+  res.send({});
 });
 
 module.exports = router;
